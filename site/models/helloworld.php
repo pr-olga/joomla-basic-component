08e58581fbbd9042586951c301dc1ad528df8978
pr-olga <frkhorkovaolga@gmail.com>
2019-04-29 23:24:28 +0200

    feat: add a variable request in the menu type

--- site/models/helloworld.php
+++ site/models/helloworld.php
@@ -31,7 +31,19 @@ class HelloWorldModelHelloWorld extends JModelItem
 	{
 		if (!isset($this->message))
 		{
-			$this->message = 'Hello World!';
+            $jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+
+			switch ($id)
+			{
+				case 2:
+					$this->message = 'Good bye World!';
+					break;
+				default:
+				case 1:
+					$this->message = 'Hello World!';
+					break;
+			}
 		}
 
 		return $this->message;
