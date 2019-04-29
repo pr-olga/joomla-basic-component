c3349b61dbb1902386787d2d820d409f16968ae9
pr-olga <frkhorkovaolga@gmail.com>
2019-04-29 23:10:34 +0200

    feat: add model to the site part

--- site/views/helloworld/view.html.php
+++ site/views/helloworld/view.html.php
@@ -19,15 +19,23 @@ class HelloWorldViewHelloWorld extends JViewLegacy
 {
 	/**
 	 * Display the Hello World view
-	 *
+	 * The HelloWorldViewHelloWorld class asks the model for data using the get method of the JViewLegacy. * * This get method converts the get('Msg') call into a getMsg() call on the model, which is the function * which we have had to provide.
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
 	 *
 	 * @return  void
 	 */
 	function display($tpl = null)
 	{
-		// Assign data to the view
-		$this->msg = 'Hello World';
+        // Assign data to the view
+        $this->msg = $this->get('Msg');
+
+        // Check for errors.
+        if (count($errors = $this->get('Errors')))
+        {
+            JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+
+            return false;
+        }
 
 		// Display the view
 		parent::display($tpl);
