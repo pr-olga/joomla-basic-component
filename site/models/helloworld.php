53ce62675444f8dea6bcd8240669c70ea1647186
pr-olga <frkhorkovaolga@gmail.com>
2019-05-04 12:44:56 +0200

    feat: add db, part 2, get on the frontend

--- site/models/helloworld.php
+++ site/models/helloworld.php
@@ -15,12 +15,13 @@ require __DIR__.'\..\vendor\autoload.php';
  *
  * @since  0.0.1
  */
+
 class HelloWorldModelHelloWorld extends JModelItem
 {
 	/**
-	 * @var string message
+	 * @var string messages
 	 */
-	protected $message;
+	protected $messages;
 
 	/**
 	 * @var string response
@@ -28,31 +29,53 @@ class HelloWorldModelHelloWorld extends JModelItem
 	protected $response;
 
 	/**
-	 * Get the message
-         *
-	 * @return  string  The message to be displayed to the user
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $type    The table name. Optional.
+	 * @param   string  $prefix  The class prefix. Optional.
+	 * @param   array   $config  Configuration array for model. Optional.
+	 *
+	 * @return  JTable  A JTable object
+	 *
+	 * @since   1.6
 	 */
-	public function getMsg()
+	public function getTable($type = 'HelloWorld', $prefix = 'HelloWorldTable', $config = array())
 	{
-		if (!isset($this->message))
-		{
-            $jinput = JFactory::getApplication()->input;
-			$id     = $jinput->get('id', 1, 'INT');
+		return JTable::getInstance($type, $prefix, $config);
+	}
 
-			switch ($id)
-			{
-				case 2:
-					$this->message = 'Good bye World!';
-					break;
-				default:
-				case 1:
-					$this->message = 'Hello World!';
-					break;
-			}
+	/**
+	 * Get the message
+	 *
+	 * @param   integer  $id  Greeting Id
+	 *
+	 * @return  string        Fetched String from Table for relevant Id
+	 */
+	public function getMsg($id = 1)
+	{
+		if (!is_array($this->messages))
+		{
+			$this->messages = array();
 		}
 
-		return $this->message;
-    }
+		if (!isset($this->messages[$id]))
+		{
+			// Request the selected id
+			$jinput = JFactory::getApplication()->input;
+			$id     = $jinput->get('id', 1, 'INT');
+
+			// Get a TableHelloWorld instance
+			$table = $this->getTable();
+
+			// Load the message
+			$table->load($id);
+
+			// Assign the message
+			$this->messages[$id] = $table->greeting;
+		}
+
+		return $this->messages[$id];
+	}
 
     public function getList()
     {
