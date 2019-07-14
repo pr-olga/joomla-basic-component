11eb3437b761fdda74d6ac1684fe44c4ee82cf13
pr-olga <frkhorkovaolga@gmail.com>
2019-07-14 13:19:19 +0200

    feat: add basic style to the list

--- site/views/helloworld/tmpl/default.php
+++ site/views/helloworld/tmpl/default.php
@@ -17,21 +17,21 @@ $document->addScript("components/com_helloworld/assets/search.js");
 ?>
 <h1><?php echo $this->msg; ?></h1>
 <h2>Our Jobs</h2>
-<div class="row search-area">
+<div class="search-area">
 <div class="col-sm-5 col-md-5 search-area__element">
 <?php echo '<input id="token" type="hidden" name="' . JSession::getFormToken() . '" value="1" />'; ?>
   <input type="text" name="alias_search" id="alias_search" class="form-control input-md" autocomplete="off" placeholder="Alias" style="">
 </div>
 <div class="col-sm-2 col-md-2 search-area__element">
 <?php echo '<input id="token" type="hidden" name="' . JSession::getFormToken() . '" value="3" />'; ?>
-  <button type="submit" class="button button--search" onclick="searchHere();">Suchen</button>
+  <button type="submit" class="btn btn-success button button--search" onclick="searchHere();">Suchen</button>
 </div>
 </div>
 
 
-<ul>
+<ul class="job-list">
 <?php foreach($this->query as $titles) :  ?>
-     <li>
+     <li class="job-list__item">
           <a href="<?php echo JRoute::_('index.php?option=com_helloworld&view=item&id=' . (int)$titles->id); ?>">
           <?php echo $titles->greeting; ?>
           </a>
